b4cfb6cb5dec567b6d067c95c583b52f8d247d16
Vaughn1262 <vaughnpalmer62@gmail.com>
2024-03-28 13:37:12 -0600

    readme

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Rust-Project
+# Overview
+
+This program was written because I want to learn Rust. Rust is different from other programming languages and has advantages such as memory management and ownership. This program has allowed me to learn and grow as a programmer
+
+The software I wrote is a password manager. It allows a user to make passwords for sites, remove those passwords, view the passwords, and see how many passwords they have stored.
+
+
+My purpose for writing this software was mostly to learn Rust and learn how the cargo tool works. It was also to get a better feel for the data structures and how they can be used in Rust.
+
+{Provide a link to your YouTube demonstration. It should be a 4-5 minute demo of the software running and a walkthrough of the code. Focus should be on sharing what you learned about the language syntax.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+
+I used Visual Studio Code as my environment and the rust extension in it. The Cargo tool comes standard with rust and is used to build the program into a running program. 
+
+I used the Rust programming language to make this program. The only library I used was the std::io library that allows for user input and output through the terminal.
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+
+- [GeeksforGeeks](https://www.geeksforgeeks.org/introduction-to-rust-programming-language/)
+- [Rust](https://www.rust-lang.org/)
+
+# Future Work
+
+- Permanent password storage through a word file
+- Security 
+- A nice looking GUI
--- passproj/src/main.rs
+++ passproj/src/main.rs
@@ -2,21 +2,32 @@ use std::io;
 
 
 fn main(){
+    //sets initial run condition
     let mut running = 1;
+
+    //Makes 2 vectors to store passwords and sites
     let mut namevec = vec![];
     let mut passvec = vec![];
 
+    //runs as long as the program isn't exited
     while running != 0 {
+
+        //displays the Menu anytime that the program isn't ended. 
         println!("1. Add a password: ");
         println!("2. Remove a password: ");
         println!("3. View a password: ");
         println!("4. View how many passwords are stored: ");
         println!("5. Quit the program: ");
+        
+        //Gets user input
         let mut optinput = String::new();
         println!("Please select an option(1-5). ");
         io::stdin().read_line(&mut optinput).expect("failed to read line");
-        let optinput = optinput.trim_end();
 
+        //Trims off any newline or space characters at the end.
+        let optinput = optinput.trim_end();
+        
+        //Adds a password and a website to associate with that password
         if optinput == "1"{
             println!("Please give the website name: ");
             let mut webinput = String::new();
@@ -26,7 +37,8 @@ fn main(){
             let mut passinput = String::new();
             io::stdin().read_line(&mut passinput).expect("failed to read line");
             passvec.push(passinput);
-
+        
+        //Allows the user to remove a certain websites password. Also removes the website.
         } else if optinput == "2"{
             println!("What websites password would you like to remove? ");
             let mut reminput = String::new();
@@ -34,7 +46,8 @@ fn main(){
             let remi = namevec.iter().position(|s| s == &reminput).unwrap();
             namevec.remove(remi);
             passvec.remove(remi);
-
+        
+        //Allows the user to view a password for a specific website.
         } else if optinput == "3"{
             println!("What websites password would you like to view? ");
             let mut viewinput = String::new();
@@ -42,14 +55,18 @@ fn main(){
             let viewi = namevec.iter().position(|s| s == &viewinput).unwrap();
             let viewpass = &passvec[viewi];
             print!("{}\n",viewpass);
-            
+
+        //Counts how many passwords the user has stored.  
         } else if optinput == "4"{
             let mut count = 0;
             for i in &namevec{
                 count += 1;
             }
+
+        // Prints how many passwords the user has stored.
             println!("{}", count);
-            
+
+        //Quits the program if the user doesn't choose 1-4   
         } else {
             println!("you quit");
             running = 0;
